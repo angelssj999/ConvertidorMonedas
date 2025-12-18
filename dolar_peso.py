7593ca43560db3c8ccf87f928903a58d3fc797d6
aixa-source <aixa8868@gmail.com>
2025-12-18 09:14:06 -0600

    subo archivo python

---
+++ dolar_peso.py
@@ -0,0 +1,4 @@
+tipo_cambio=17
+dolares=float(input("ingresa la cantidad en dolares:"))
+pesos=dolares*tipo_cambio
+print("equivale a",pesos,"dolares")
---
+++ peso_dolar.py
@@ -0,0 +1,4 @@
+tipo_cambio=17
+pesos=float(input("ingresa la cantidad en pesos:"))
+dolares=pesos/tipo_cambio
+print("equivale a",dolares,"dolares")
